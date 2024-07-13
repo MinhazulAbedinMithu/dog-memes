a03985460a22088c08688b5a75ba0ae7be7fe1be
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2024-07-13 13:48:25 +0600

    footer social links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,19 +7,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Footer = () => {
-    const link = ""
+    const twitterLink = "https://twitter.com/ChaseOnETH";
+    const telegramLink =  "https://t.me/ChaseOnETH"
+    // const websiteLink =  "Website: https://ChaseOnETH.fun"
   return (
     <div className='blue-bg flex items-center justify-center gap-3 py-5'>
-        <Link href={link}>
+        <Link href={twitterLink}>
             <Image src={imgTwitter} alt='twitter' className='w-10'/>
         </Link>
-        <Link href={link}>
+        <Link href={telegramLink}>
             <Image src={imgTelegram} alt='telegram' className='w-12'/>
         </Link>
-        <Link href={link}>
+        <Link href={telegramLink}>
             <Image src={imgDexscreener} alt='dexscreener' className='w-10'/>
         </Link>
-        <Link href={link}>
+        <Link href={telegramLink}>
             <Image src={imgDextools} alt='dextools' className='w-14'/>
         </Link>
     </div>
