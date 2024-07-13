2c215044420c68706cf01da00964ebba1ec622b6
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2024-07-13 13:47:35 +0600

    added logo and social links

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import imgSocial from "@/public/IMG_social.png"
 import imgBuy from "@/public/hero-buy.png"
-import imgChase from "@/public/hero-chase.png"
+import imgChase from "@/public/chase-on-eth.png"
 import Image from 'next/image';
 import Link from 'next/link';
 import imgTwitter from "@/public/twitter.png"
@@ -10,12 +10,14 @@ import imgDexscreener from "@/public/dexscreener.svg"
 import imgDextools from "@/public/dextools.png"
 
 const Hero = () => {
-  const link = ""
+    const twitterLink = "https://twitter.com/ChaseOnETH";
+    const telegramLink =  "https://t.me/ChaseOnETH"
+    // const websiteLink =  "Website: https://ChaseOnETH.fun"
   return (
     <div className={`hero-bg w-full min-h-[100vh] h-full relative bg-cover bg-[72%] md:bg-bottom`}>
 
-        <Link href={""}  className='absolute top-2 left-12 md:left-4'>
-            <Image src={imgChase} alt='Chase' className='max-w-[280px] md:max-w-[450px] w-full' />
+        <Link href={""}  className='absolute top-2 left-20 md:left-4'>
+            <Image src={imgChase} alt='Chase' className='max-w-[220px] md:max-w-[320px] w-full' />
         </Link>
 
         <Link href={""}  className='absolute top-[20%] md:top-2 right-[23%] md:right-4'>
@@ -26,16 +28,16 @@ const Hero = () => {
             <Image src={imgSocial} alt='Social Icons' className='w-[100px] md:w-auto' />
         </Link> */}
         <div className='absolute top-[55%] translate-y-[-45%] right-4 flex items-center justify-center gap-3 flex-col blue-bg px-2 py-4 rounded-full shadow-xl drop-shadow-xl border-4 border-black'>
-          <Link href={link}>
+          <Link href={twitterLink}>
               <Image src={imgTwitter} alt='twitter' className='w-10'/>
           </Link>
-          <Link href={link}>
+          <Link href={telegramLink}>
               <Image src={imgTelegram} alt='telegram' className='w-12'/>
           </Link>
-          <Link href={link}>
+          <Link href={telegramLink}>
               <Image src={imgDexscreener} alt='dexscreener' className='w-10'/>
           </Link>
-          <Link href={link}>
+          <Link href={telegramLink}>
               <Image src={imgDextools} alt='dextools' className='w-14'/>
           </Link>
         </div>
